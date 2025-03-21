3f1b43531744af76e2fe580b5dba1e4146c64e3c
master <mondols98@naver.com>
2025-03-21 16:18:22 +0900

    feat : ResumeDTO 작성

---
+++ src/main/java/com/jobhunter/model/resume/ResumeDTO.java
@@ -0,0 +1,10 @@
+package com.jobhunter.model.resume;
+
+public class ResumeDTO {
+	private String title;
+	private String payType;
+	private int pay;
+	private String introduce;
+	private String saveType;
+	private String userPk;
+}
