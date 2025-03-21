3cb2dfa82ec58f7e21607fd1b29e4ad906b68e24
master <mondols98@naver.com>
2025-03-21 16:40:47 +0900

    fix : ResumeDTO 수정

--- src/main/java/com/jobhunter/model/resume/ResumeDTO.java
+++ src/main/java/com/jobhunter/model/resume/ResumeDTO.java
@@ -1,10 +1,11 @@
 package com.jobhunter.model.resume;
 
 public class ResumeDTO {
+	private int resumeNo;
 	private String title;
 	private String payType;
 	private int pay;
 	private String introduce;
 	private String saveType;
-	private String userPk;
+	private int userPk;	
 }
