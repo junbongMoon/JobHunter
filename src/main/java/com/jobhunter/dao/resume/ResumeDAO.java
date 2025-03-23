3fe09be1daae47b4341c2eef8aaab2f9aa94c7fb
master <mondols98@naver.com>
2025-03-23 20:38:02 +0900

    feat : 이력서 임시저장 + 최종저장, 카테고리 불러오기, 희망근무지역, 고용형태, 희망 업직종 저장하기 기능 DAO 단 작성

--- src/main/java/com/jobhunter/dao/resume/ResumeDAO.java
+++ src/main/java/com/jobhunter/dao/resume/ResumeDAO.java
@@ -1,5 +1,32 @@
 package com.jobhunter.dao.resume;
 
-public interface ResumeDAO {
+import java.util.List;
 
+import com.jobhunter.model.resume.JobFormDTO;
+import com.jobhunter.model.resume.MajorCategoryDTO;
+import com.jobhunter.model.resume.RegionDTO;
+import com.jobhunter.model.resume.ResumeDTO;
+import com.jobhunter.model.resume.SigunguDTO;
+import com.jobhunter.model.resume.SubCategoryDTO;
+
+public interface ResumeDAO {
+	void insertResumeTemp(ResumeDTO resumeDTO);
+
+	void insertResumeFinal(ResumeDTO resumeDTO);
+
+	void insertJobForm(JobFormDTO jobFormDTO);
+
+	void insertSigungu(int resumeNo, int sigunguNo);
+
+	void insertMajorCategory(int resumeNo, int majorNo);
+
+	void insertSubCategory(int resumeNo, int subcategoryNo);
+
+	List<RegionDTO> selectAllRegions();
+
+	List<SigunguDTO> selectSigunguByRegion(int regionNo);
+
+	List<MajorCategoryDTO> selectAllMajorCategories();
+
+	List<SubCategoryDTO> selectSubCategoriesByMajor(int majorcategoryNo);
 }
---
+++ src/main/java/com/jobhunter/dao/resume/ResumeDAOImpl.java
@@ -0,0 +1,86 @@
+package com.jobhunter.dao.resume;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.jobhunter.model.resume.JobFormDTO;
+import com.jobhunter.model.resume.MajorCategoryDTO;
+import com.jobhunter.model.resume.RegionDTO;
+import com.jobhunter.model.resume.ResumeDTO;
+import com.jobhunter.model.resume.SigunguDTO;
+import com.jobhunter.model.resume.SubCategoryDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ResumeDAOImpl implements ResumeDAO {
+
+	private final SqlSession ses;
+
+	private static final String NS = "com.jobhunter.mapper.resumemapper";
+
+	@Override
+	public void insertResumeTemp(ResumeDTO resumeDTO) {
+		ses.insert(NS + ".insertResumeTemp", resumeDTO);
+	}
+
+	@Override
+	public void insertResumeFinal(ResumeDTO resumeDTO) {
+		ses.insert(NS + ".insertResumeFinal", resumeDTO);
+	}
+
+	@Override
+	public void insertJobForm(JobFormDTO jobFormDTO) {
+		ses.insert(NS + ".insertJobForm", jobFormDTO);
+	}
+
+	@Override
+	public void insertSigungu(int resumeNo, int sigunguNo) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("resumeNo", resumeNo);
+		map.put("sigunguNo", sigunguNo);
+		ses.insert(NS + ".insertSigungu", map);
+	}
+
+	@Override
+	public void insertMajorCategory(int resumeNo, int majorNo) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("resumeNo", resumeNo);
+		map.put("majorNo", majorNo);
+		ses.insert(NS + ".insertMajorCategory", map);
+	}
+
+	@Override
+	public void insertSubCategory(int resumeNo, int subcategoryNo) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("resumeNo", resumeNo);
+		map.put("subcategoryNo", subcategoryNo);
+		ses.insert(NS + ".insertSubCategory", map);
+	}
+
+	@Override
+	public List<RegionDTO> selectAllRegions() {
+		return ses.selectList(NS + ".selectAllRegions");
+	}
+
+	@Override
+	public List<SigunguDTO> selectSigunguByRegion(int regionNo) {
+		return ses.selectList(NS + ".selectSigunguByRegion", regionNo);
+	}
+
+	@Override
+	public List<MajorCategoryDTO> selectAllMajorCategories() {
+		return ses.selectList(NS + ".selectAllMajorCategories");
+	}
+
+	@Override
+	public List<SubCategoryDTO> selectSubCategoriesByMajor(int majorcategoryNo) {
+		return ses.selectList(NS + ".selectSubCategoriesByMajor", majorcategoryNo);
+	}
+
+}
