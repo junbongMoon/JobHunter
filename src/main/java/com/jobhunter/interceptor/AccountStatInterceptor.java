d4b87fa2217af927fe11a9d1b2815e8b09141844
YukGeunWoo <dbrrmsdn51@naver.com>
2025-03-27 22:19:36 +0900

    feat : 정지/삭제대기 유저 체크 인터셉터 개별로 작동시킬수있는 유연성 추가 및 인터셉터 설명서 보강강

--- src/main/java/com/jobhunter/interceptor/AccountStatInterceptor.java
+++ src/main/java/com/jobhunter/interceptor/AccountStatInterceptor.java
@@ -40,27 +40,46 @@ public class AccountStatInterceptor implements HandlerInterceptor {
         
         // 일단 계정 상태 서버에있는걸로 갱신좀 해서 그사이 정지먹진않았나 체크해주고
         AccountVO account = accUtils.refreshAccount((AccountVO) session.getAttribute("account"));
+        
+        String checkStatType = request.getParameter("checkStatType");
+        
+        // 유연성 추가용 쿼리스트링(계정정지/계정삭제 단일체크) 확인
+        boolean checkBlock = checkStatType == null || "BLOCK".equalsIgnoreCase(checkStatType);
+        boolean checkDelete = checkStatType == null || "DELETE".equalsIgnoreCase(checkStatType);
+
+        
 
         // 상태 확인
         boolean isBlocked = account.getBlockDeadline() != null &&
                 account.getBlockDeadline().toInstant().isAfter(Instant.now());
         boolean isDeleted = account.getDeleteDeadline() != null;
 
-        if (!isBlocked && !isDeleted) {
-            return true; // 문제 없으면 통과
+        // 문제 사유 저장할곳
+        String reason = null;
+
+        if (checkStatType == null) { // 둘다 체크할거냐(기본)
+            if (isBlocked) { // 사유 저장하기
+            	reason = "blocked"; // 삭제 대기보단 정지상태 우선 알림
+            } else if (isDeleted) {
+            	reason = "deleted"; // 사실 그냥 실패!같은것만 띄워도 상관없긴한데 기왕 쿼리 붙이는김에 넣는거라
+            }
+        } else if ("BLOCK".equalsIgnoreCase(checkStatType) && isBlocked) { // 쿼리로 붙인게 있으면 그것만 체크
+            reason = "blocked";
+        } else if ("DELETE".equalsIgnoreCase(checkStatType) && isDeleted) {
+            reason = "deleted";
+        }
+        
+        if (reason == null) {
+        	return true; // 문제없으면 통과
         }
 
         // 문제있으면 기존페이지 링크 저장하고 쿼리스트링에 에러붙여서 돌려보내기
         String uri = request.getRequestURI();
         String query = request.getQueryString();
-        String errorParam = "accessFail=" + (isBlocked ? "blocked" : "deleted");
 
-        String fullRedirectUrl;
-        if (query == null || query.isEmpty()) {
-            fullRedirectUrl = uri + "?" + errorParam;
-        } else {
-            fullRedirectUrl = uri + "?" + query + "&" + errorParam;
-        }
+        String fullRedirectUrl = (query == null || query.isEmpty())
+                ? uri + "?accessFail=" + reason
+                : uri + "?" + query + "&accessFail=" + reason;
 
         response.sendRedirect(fullRedirectUrl);
         return false;
