16021188088aea16fc45cde682abacd7a72303c8
sungbin <tjdqlszld@naver.com>
2025-04-02 01:22:14 +0900

    공고글 조회 리스트 DTO 생성

---
+++ src/main/java/com/jobhunter/model/reviewboard/RecruitmentnoticContentDTO.java
@@ -0,0 +1,26 @@
+package com.jobhunter.model.reviewboard;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder // 객체를 만들 때 정해진 패턴대로 생성하도록 Builder 패턴을 사용
+@Getter
+@Setter
+@ToString
+public class RecruitmentnoticContentDTO {
+	 private int recruitmentnoticeNo;   // 공고 ID
+	    private String recruitmentTitle;   // 공고 제목
+	    private String companyName;        // 회사 이름
+	    private String workType;           // 근무 형태
+	    private String personalHistory;    // 경력 사항
+	    private String payType;            // 급여 형태
+	    private String period;             // 근무 기간
+
+}
