15a408d210cf1092a9db5646fa2263725abb2578
junbong <moonjoon1212@gmail.com>
2025-04-02 17:13:24 +0900

    fix : 공고를 저장할 때 추가로 필요한 필드 작성

--- src/main/java/com/jobhunter/controller/recruitmentnotice/RecruitmentNoticeController.java
+++ src/main/java/com/jobhunter/controller/recruitmentnotice/RecruitmentNoticeController.java
@@ -1,5 +1,8 @@
 package com.jobhunter.controller.recruitmentnotice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jobhunter.model.page.PageRequestDTO;
 import com.jobhunter.model.page.PageResponseDTO;
+import com.jobhunter.model.recruitmentnotice.AdvantageDTO;
+import com.jobhunter.model.recruitmentnotice.ApplicationDTO;
 import com.jobhunter.model.recruitmentnotice.RecruitmentDetailInfo;
+import com.jobhunter.model.recruitmentnotice.RecruitmentnoticeBoardUpfiles;
 import com.jobhunter.service.recruitmentnotice.RecruitmentNoticeService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,22 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/recruitmentnotice")
 public class RecruitmentNoticeController {
 	private final RecruitmentNoticeService recruitmentService;
+	
+	// 양식을 저장 할 List, 코드들
+		private final List<AdvantageDTO> advantageList = new ArrayList<>();
+		private final List<ApplicationDTO> applicationList = new ArrayList<>();
+		private String regionCode;
+		private String sigunguCode;
+		private String majorCategoryCode;
+		private String subCategoryCode;
+		// 게시글 작성시 업로드한 파일객체들을 임시로 저장
+		private List<RecruitmentnoticeBoardUpfiles> fileList = new ArrayList<RecruitmentnoticeBoardUpfiles>();
+		// 게시글 수정시 업로한 파일 객체들을 임식로 저장
+		private List<RecruitmentnoticeBoardUpfiles> modifyFileList;
+	
+	// 공고를 저장하는 메서드
+	
+	
 	
 	// 전체 공고 리스트를 출력하는 메서드  
 	@GetMapping("/listAll")
