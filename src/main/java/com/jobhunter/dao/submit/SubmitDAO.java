f8416c0ece085f34aa5f042b39a6c57718532239
JunBong Moon <moonjoon1212@gmail.com>
2025-04-08 23:27:06 +0900

    feat : 제출 된 이력서를 조회하는 기능 DAO 작성

--- src/main/java/com/jobhunter/dao/submit/SubmitDAO.java
+++ src/main/java/com/jobhunter/dao/submit/SubmitDAO.java
@@ -1,5 +1,17 @@
 package com.jobhunter.dao.submit;
 
-public interface SubmitDAO {
+import java.util.List;
 
+import com.jobhunter.model.page.PageResponseDTO;
+import com.jobhunter.model.submit.ResumeDetailInfoBySubmit;
+
+public interface SubmitDAO {
+	
+	// 공고 PK로 제출한 이력서들을 페이징해서 담아오는 메서드
+	List<ResumeDetailInfoBySubmit> selectResumDetailInfoBySubmitByRecruitmentUid(int RecruitmentUid, 
+			PageResponseDTO<ResumeDetailInfoBySubmit> pageResponseDTO) throws Exception;
+	
+	// 공고에 제출한 이력서 row수를 가져오는 메서드
+	int selectTotalCountRowOfResumeByUid(int uid) throws Exception;
+	
 }
---
+++ src/main/java/com/jobhunter/dao/submit/SubmitDAOImpl.java
@@ -0,0 +1,43 @@
+package com.jobhunter.dao.submit;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.jobhunter.model.page.PageResponseDTO;
+import com.jobhunter.model.submit.ResumeDetailInfoBySubmit;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class SubmitDAOImpl implements SubmitDAO {
+	
+	private SqlSession ses;
+	
+	private String NS = "com.jobhunter.mapper.submitmapper";
+	
+
+
+    @Override
+    public List<ResumeDetailInfoBySubmit> selectResumDetailInfoBySubmitByRecruitmentUid(int recruitmentUid,
+            PageResponseDTO<ResumeDetailInfoBySubmit> pageResponseDTO) throws Exception {
+        
+        Map<String, Object> params = new HashMap<>();
+        params.put("recruitmentUid", recruitmentUid);
+        params.put("startRowIndex", pageResponseDTO.getStartRowIndex());
+        params.put("rowCntPerPage", pageResponseDTO.getRowCntPerPage());
+
+        return ses.selectList(NS + ".selectRecruitmentsByRecruitmentUid", params);
+    }
+
+	@Override
+	public int selectTotalCountRowOfResumeByUid(int uid) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
