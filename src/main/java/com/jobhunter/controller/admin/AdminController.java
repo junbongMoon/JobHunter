89584ca069b9fb3d8ba7cf60fe2aca3899e7491b
master <mondols98@naver.com>
2025-04-14 23:19:12 +0900

    feat : admin 유저관리를 위한 jsp생성 + 헤더에 링크 추가 / 컨트롤러 매핑까지 완료

--- src/main/java/com/jobhunter/controller/admin/AdminController.java
+++ src/main/java/com/jobhunter/controller/admin/AdminController.java
@@ -4,11 +4,11 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
 public class AdminController {
 	
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
@@ -16,4 +16,17 @@ public class AdminController {
 
 		return "admin/adminhome";
 	}
+	
+	@GetMapping("/admin/userList")
+	public String adminUserList(Locale locale, Model model) {
+		return "admin/adminUserList";
+	}
+	
+	@GetMapping("/admin/userBlockList")
+	public String adminUserBlockList(Locale locale, Model model) {
+		return "admin/adminUserBlockList";
+	}
+	
+	
+
 }
---
+++ src/main/webapp/WEB-INF/views/admin/adminUserList.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>관리자 페이지</title>
+</head>
+<body id="page-top">
+
+	<!-- 헤더 포함 (네비게이션바 + 사이드바 포함) -->
+	<jsp:include page="adminheader.jsp"></jsp:include>
+
+	<!-- 본문 내용 -->
+	<div class="container-fluid">
+		<h1 class="h3 mb-4 text-gray-800">유저 목록 예정</h1>
+	</div>
+
+	<!-- 푸터 포함 -->
+	<jsp:include page="adminfooter.jsp"></jsp:include>
+
+</body>
+</html>
