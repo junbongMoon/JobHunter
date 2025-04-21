7cf830417acc8198c6a8e11d5c0f0235457c5013
kindergarten <mondols98@naver.com>
2025-04-21 15:41:54 +0900

    feat : 이력서 첨삭 저장을 위한 DTO

---
+++ src/main/java/com/jobhunter/model/resume/ResumeAdviceDTO.java
@@ -0,0 +1,25 @@
+package com.jobhunter.model.resume;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeAdviceDTO {
+    private int adviceNo;
+    private int resumeNo;
+    private String adviceContent;
+    private LocalDateTime regDate;
+    private List<ResumeAdviceUpfileDTO> files;
+} 
---
+++ src/main/java/com/jobhunter/model/resume/ResumeAdviceUpfileDTO.java
@@ -0,0 +1,23 @@
+package com.jobhunter.model.resume;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeAdviceUpfileDTO {
+    private int adviceUpfileNo;
+    private int adviceNo;
+    private String adviceFileName;
+    private LocalDateTime regDate;
+} 
