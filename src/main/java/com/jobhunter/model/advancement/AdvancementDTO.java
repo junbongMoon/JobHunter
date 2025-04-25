f3ef6f56178b0847387febe997e5b02795db8a35
junbong <moonjoon1212@gmail.com>
2025-04-25 09:46:47 +0900

    feat : AdvancementDTO 작성

---
+++ src/main/java/com/jobhunter/model/advancement/AdvancementDTO.java
@@ -0,0 +1,19 @@
+package com.jobhunter.model.advancement;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class AdvancementDTO {
+	private int advancementNo;
+	private String title;
+	private String writer;
+	private int refUser;
+	private String content;
+}
