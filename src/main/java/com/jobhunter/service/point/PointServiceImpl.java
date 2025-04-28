bb573acd2564331ed7485ae1156ba3dad462f925
kindergarten <mondols98@naver.com>
2025-04-28 12:32:52 +0900

    fix : 불필요 메서드 주석처리

--- src/main/java/com/jobhunter/service/point/PointServiceImpl.java
+++ src/main/java/com/jobhunter/service/point/PointServiceImpl.java
@@ -21,17 +21,18 @@ public class PointServiceImpl implements PointService {
     @Override
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = Exception.class)
     public void submitAdvicePointLog(int mentorUid, int sessionUid, int point, int rgAdviceNo) throws Exception {
-        try {
-            // user -1000 포인트 차감
-            int updateResult = userDAO.updateUserPoint(sessionUid, point);
-            if (updateResult <= 0) {
-                throw new Exception("포인트 차감에 실패했습니다.");
-            }
-
-            // 포인트 로그 저장
-            pointDAO.submitAdvicePointLog(mentorUid, sessionUid, point, rgAdviceNo);
-        } catch (Exception e) {
-            throw new Exception("포인트 처리 중 오류가 발생했습니다: " + e.getMessage());
-        }
+//        try {
+//            // user -1000 포인트 차감
+//            int updateResult = userDAO.updateUserPoint(sessionUid, point);
+//            if (updateResult <= 0) {
+//                throw new Exception("포인트 차감에 실패했습니다.");
+//            }
+//
+//            // 포인트 로그 저장
+//            pointDAO.submitAdvicePointLog(mentorUid, sessionUid, point, rgAdviceNo);
+//        } catch (Exception e) {
+//            throw new Exception("포인트 처리 중 오류가 발생했습니다: " + e.getMessage());
+//        }
+//    }
     }
 }
